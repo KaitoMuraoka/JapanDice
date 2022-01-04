542fa2ddd0063c698a356047c3dab780f5721405
KaitoMuraoka <kaitoboshi@icloud.com>
2022-01-04 22:48:06 +0900

    サイドメニューの廃止とサイコロを振る時にアニメーションを流すようにしました

--- JapanDice/JapanDice/ViewControllerControllers/MainViewController.swift
+++ JapanDice/JapanDice/ViewControllerControllers/MainViewController.swift
@@ -24,12 +24,6 @@ class MainViewController: UIViewController, EAIntroDelegate {
         super.viewDidLoad()
         
         
-        menu = SideMenuNavigationController(rootViewController: MenuListController())
-        menu?.leftSide = true
-        menu?.setNavigationBarHidden(true, animated: false)
-        
-        SideMenuManager.default.leftMenuNavigationController = menu
-        SideMenuManager.default.addPanGestureToPresent(toView: self.view)
     }
     
     @IBAction func didTapMenu(){
@@ -130,35 +124,3 @@ class MainViewController: UIViewController, EAIntroDelegate {
     
 }
 
-//MARK: -SideMenu
-class MenuListController: UITableViewController{
-    let darkColor = UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
-    
-    var items = ["新幹線　運行状況", "飛行機　運行状況", "コロナ　感染状況", "使い方"]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.backgroundColor = darkColor
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = items[indexPath.row]
-        cell.textLabel?.textColor = .white
-        cell.backgroundColor = darkColor
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        //select menu
-        
-    }
-    
-}
